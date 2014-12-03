16374f0d8ba6c753bd7d7c8ff0216d732254db61
fpizzurro <felice.pizzurro@softecspa.it>
2014-12-03 10:26:19 +0000

    fix

---
+++ manifests/varnish.pp
@@ -0,0 +1,33 @@
+class softec_php::varnish {
+
+  require softec_php
+  require softec_php::pear
+  include ::php::params
+  $php_varnish_version = hiera('php_varnish_version',undef)
+
+  $ensure = $php_varnish_version?{
+    undef   => present,
+    default => $php_varnish_version
+  }
+
+  php::extension {'varnish':
+    ensure    => $ensure,
+    provider  => 'pecl',
+    package   => 'varnish-1.1.1',
+    require   => Package['libvarnishapi-dev']
+  } ->
+
+  file {"${::php::params::config_root_ini}/varnish.ini":
+    ensure  => present,
+    mode    => 644,
+    owner   => 'root',
+    group   => 'root',
+    content => '; GENERATED WITH PUPPET\nextension=varnish.so',
+  } ->
+
+  file {'/etc/php5/conf.d/varnish.ini':
+    ensure  => link,
+    target  => "${::php::params::config_root_ini}/varnish.ini",
+  }
+
+}
