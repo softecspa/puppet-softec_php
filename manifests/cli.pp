c402df8ad550652e953b54b557aca9019e5bf3ec
Lorenzo Cocchi <lorenzo.cocchi@softecspa.it>
2015-02-06 16:07:46 +0100

    fix

--- manifests/cli.pp
+++ manifests/cli.pp
@@ -10,10 +10,11 @@ class softec_php::cli {
     default => php_hash2array($php_cli_settings)
   }
 
-  softec_php::pin {'php5-cli':} ->
+  softec_php::pin {'php5-cli':}
   include php::cli
   #class{'php::cli':
   #  ensure    => $php_version,
   #  settings  => $array_php_cli_settings
   #}
+  Softec_php::Pin['php5-cli'] -> Class['php::cli'] 
 }
